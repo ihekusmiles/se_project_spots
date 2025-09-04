d1774abee668d39a065a8387fa1d4e5571a75be2
ihekusmiles <hrobles@buffalo.edu>
2025-09-03 20:50:17 -0400

    Added validation logic for form inputs

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,19 +1,55 @@
 const settings = {
   formSelector: ".modal__form",
-  inputSelector: "modal__input",
-  submitButtonSelector: ".modal__save-btn",
-  inactiveButtonClass: ".modal__button_disabled",
-  inputErrorClass: ".mmodal__input_type_error",
-  errorClass: "modal__error_visible",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__save-btn", // save button = submit button
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "modal__input_type_error", // input field itself <input>-> eg. border color
+  errorClass: "modal__error_visible", // controls error visibility, applied to <span>
 };
 
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  console.log(errorElement);
+  inputElement.classList.add(settings.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(settings.errorClass);
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove(settings.inputErrorClass);
+  errorElement.classList.remove(settings.errorClass);
+  errorElement.textContent = "";
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    //if valid = false (not true) then make error message appear
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+// For each input -> add event listener that calls checkInputValidity()
+const setEventListeners = (formElement, config) => {
+  const inputList = formElement.querySelectorAll(config.inputSelector);
+  const buttonElement = formElement.querySelectorAll(
+    config.submitButtonSelector
+  );
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formElement, inputElement);
+    });
+  });
+};
+
+// Get all forms -> loop through each form and set even listener
 const enableValidation = (config) => {
   const formList = Array.from(document.querySelectorAll(config.formSelector));
   formList.forEach((formElement) => {
     setEventListeners(formElement, config);
   });
 };
-
-const setEventListeners = (formElement, config) => {};
 
 enableValidation(settings); // Object "settings" gets passed to config
